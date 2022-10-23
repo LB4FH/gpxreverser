38888581400bdcdde2360f5580a085eb81dee911
LB4FH <48948238+LB4FH@users.noreply.github.com>
2022-10-23 09:28:43 +0200

    Added comments (#6)
    * Fixed README formatting
    
    * Added comments (#5)

--- Program.cs
+++ Program.cs
@@ -8,39 +8,63 @@ namespace LB4FH.GpxReverser
     {
         static void Main(string[] args)
         {
+            #region Input validation
             if (string.IsNullOrEmpty(args[0]))
             {
-                Console.WriteLine("Error: specify file to reverse:  'gpxreverser.exe sometrack.gpx'");
+                Console.WriteLine("Error, no input file specified");
+                Console.WriteLine("Start application with: 'gpxreverser.exe track.gpx'");
+                return;
+            }
+            if (args.Length> 1)
+            {
+                Console.WriteLine("Error, too many arguments");
+                Console.WriteLine("Start application with: 'gpxreverser.exe track.gpx'");
                 return;
             }
             if (!args[0].EndsWith(".gpx"))
             {
-                Console.WriteLine("Error: input filename must end with .gpx.");
+                Console.WriteLine("Error: input filename must end with .gpx");
                 return;
             }
+            #endregion
 
             string inputFileName = args[0];
             string outputFileName = inputFileName.Replace(".gpx", string.Empty) + "-reversed.gpx";
 
             var xmlData = string.Empty;
+            
             using (StreamReader sr = new StreamReader(inputFileName))
             {
                 xmlData = sr.ReadToEnd();
             }
+            
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(xmlData);
+
+
             var track = new GpxTrack();
 
-            var elems = doc.GetElementsByTagName("trk");
+            // Gpx files are separated into elements called "Track segments", with the shortname trk
+            var elems = doc.GetElementsByTagName("trk");          
+            
             Console.WriteLine($"Found {elems.Count} track segments");
+
+            // Iterate over the segments to get different values
             foreach (XmlElement curSegment in elems.Item(0))
             {
+
+                // Name is commonly used as the display name
                 if (curSegment.Name == "name")
                 {
                     track.Name = curSegment.InnerText;
                 }
+
+                // Each GPX file consists of one or more segments. These segments are usually set up automatically by the track logging software.
+                // For simplicity this program combines all track segments into one segment in the reversed track.
                 if (curSegment.Name == "trkseg")
                 {
+                    // Each point consists of at least four elements; latitude, longtitude, elevation and time.
+                    // The GPX sources I've seen all make these the same way, but there may be some that reverse elevation or time, so the code below here should be improved to handle that.
                     foreach (XmlElement curPoint in curSegment.ChildNodes)
                     {
                         var tp = new TrackPoint()
@@ -55,6 +79,7 @@ namespace LB4FH.GpxReverser
                 }
             }
             Console.WriteLine($"Found {track.points.Count} points");
+            
             try
             {
                 track.Save(outputFileName);
